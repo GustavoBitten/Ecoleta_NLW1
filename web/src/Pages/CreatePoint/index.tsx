f859f42fcd8d2910b9cf25d7411e6c2c03063876
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-06-06 18:02:45 -0300

    HomePage and CreatePointPage

--- web/src/Pages/CreatePoint/index.tsx
+++ web/src/Pages/CreatePoint/index.tsx
@@ -1,12 +1,54 @@
-import React from 'react'
+import React,{useEffect,useState,ChangeEvent} from 'react'
 import {FiArrowLeft} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
+import {Map,TileLayer , Marker} from 'react-leaflet'
+import api from '../../services/api'
+import axios from 'axios'
 
 import logo from '../../assets/logo.svg'
 
 import './style.css'
 
+interface Item {
+    id: number,
+    title: string,
+    image_url: string
+}
+interface IBGEUFres {
+    sigla:string
+}
+
+
 const CreatePoint = () =>{
+
+    const [items,setItems] = useState<Item[]>([])
+    const [ufs,setUfs] = useState<string[]>([])
+    const [selectdUf,setSelectdUf] = useState('0')
+
+    useEffect(()=>{
+        api.get('items').then(res =>{
+            setItems(res.data)
+        })
+
+    },[])
+
+    useEffect(()=>{
+        axios.get<IBGEUFres[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(res =>{
+            const ufInitials = res.data.map(uf => uf.sigla)
+
+            setUfs(ufInitials)
+        })
+    },[])
+
+    useEffect(()=>{
+
+    },[])
+
+    function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+        const uf = event.target.value
+
+        setSelectdUf(uf)
+    }
 
     return (
        <div id="page-create-point">
@@ -50,11 +92,25 @@ const CreatePoint = () =>{
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
+
+                    <Map center={[-27.2092052,-49.6401092]} zoom={15}>
+                        <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-27.2092052,-49.6401092]}/>
+                       
+                    
+                    </Map> 
+
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado (UF)</label>
                             <select name="uf" id="uf">
                                 <option value="0">Selecione uma UF</option>
+                                {ufs.map(uf =>(
+                                    <option key={uf} value={uf}>{uf}</option>
+                                ))}
                             </select>
                         </div>
                         <div className="field">
@@ -75,30 +131,13 @@ const CreatePoint = () =>{
                     </legend>
 
                     <ul className="items-grid">
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
+                    { items.map(item =>(
+                        <li key={item.id}>
+                            <img src={item.image_url} alt={item.title}/>
+                            <span>{item.title}</span>
                         </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
+                    ))}
+                        
                     </ul>
                 </fieldset>
 
